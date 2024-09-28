f20876f68b444bcee761882bbb0d3a5cb799c22e
Liam Palmqvist <Liam.palmqvist@icloud.com>
2024-09-28 22:16:49 +0100

    Finished anime and manga search

--- commands/utility/searchanime.js
+++ commands/utility/searchanime.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder, StringSelectMenuBuilder, ActionRowBuilder, ButtonBuilder} = require('@discordjs/builders');
+const {SlashCommandBuilder, StringSelectMenuBuilder, ActionRowBuilder, EmbedBuilder, ButtonBuilder} = require('@discordjs/builders');
 const { MAL_KEY } = require('../../config.json');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
@@ -14,14 +14,15 @@ module.exports = {
                     .setDescription('The anime title to search for')
                     .setRequired(true)
             )
-            .addIntegerOption(option =>
+            /*.addIntegerOption(option =>
                 option.setName('amount')
                     .setDescription('The amount of results to return')
                     .setRequired(true)
                     .setMaxValue(10)
                     .setMinValue(1)
-            )
+            )*/
         )
+
         .addSubcommand(subcommand =>
             subcommand.setName('manga')
             .setDescription('Search for a manga')
@@ -30,6 +31,7 @@ module.exports = {
                     .setDescription('The manga title to search for')
                     .setRequired(true)
             )
+            /*
             .addIntegerOption(option =>
                 option.setName('amount')
                     .setDescription('The amount of results to return')
@@ -37,6 +39,7 @@ module.exports = {
                     .setMaxValue(10)
                     .setMinValue(1)
             )
+            */
         ),
     async execute(interaction) {
         
@@ -44,11 +47,19 @@ module.exports = {
         
         const data = await search(interaction.options.getString("title"), interaction.options.getInteger("amount"), `${interaction.options.getSubcommand()}?`);
         console.log(data);
-        let message = ""
-        if (data.data.length > 0) {
+        const message = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle(`Search results for ${interaction.options.getString("title")}`)
+            .setTimestamp()
+            .setFooter({ text: 'Powered by MyAnimeList', iconURL: 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'});
+
+        if (await data.data.length > 0) {
             for (let i = 0; i < data.data.length; i++) {
-                message+=(`${i}. ${data.data[i].node.title}\n`);
+                message.addFields(
+                    { name: ` `, value: `${i+1}. ${data.data[i].node.title}`, inline: false }
+                )
             }
+            
         }
 
         /*
@@ -79,7 +90,7 @@ module.exports = {
             //.addComponents(arrowRight);
 
         const response = await interaction.reply({
-            content: `${message}`, 
+            embeds: [message], 
             components: [row],
         });
 
@@ -90,20 +101,83 @@ module.exports = {
             const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
             console.log(confirmation.values[0]);
             const entry = await grabEntry(data.data[confirmation.values[0]].node.id, `${interaction.options.getSubcommand()}`);
-            console.log(entry);
-            await interaction.editReply({ content: `Title: ${entry.title}\nRank: ${entry.rank}\Score: ${entry.mean}\nAlternative Title: ${entry.alternative_title}`, components: [] });
+
+            // ================== Embed Builder ==================
+            
+            const message = new EmbedBuilder()
+                .setColor(0x0099ff)
+                .setTitle(`${entry.title}`)
+                .setURL(`https://myanimelist.net/${interaction.options.getSubcommand()}/${entry.id}`)
+                .setThumbnail(entry.main_picture.large??entry.main_picture.medium??entry.main_picture.small)
+                .addFields(
+                    { name: 'Alternate titles', value: `${entry.alternative_titles.ja}`, inline: true },
+                    { name: 'Rank', value: `${entry.rank === undefined ? "Unranked" : entry.rank}`, inline: true },
+                    { name: 'Score', value: `${entry.mean === undefined ? "No score" : entry.mean}`, inline: true },
+                    { name: 'Start Date', value: `${entry.start_date===undefined ? "TDA" : entry.start_date}`, inline: true }
+                )
+                .setTimestamp()
+                .setFooter({ text: 'Powered by MyAnimeList', iconURL: 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'});
+
+            if (entry.end_date !== undefined) {   
+                switch (interaction.options.getSubcommand()) {
+                case 'anime':
+                    message.addFields(
+                        { name: 'End Date', value: `${entry.end_date}`, inline: true },
+                        { name: 'Episodes', value: `${entry.num_episodes}`, inline: true },
+                    )
+                    break;
+                case 'manga':
+                    message.addFields(
+                        { name: 'End Date', value: `${entry.end_date}`, inline: true },
+                        { name: 'Chapters', value: `${entry.num_chapters}`, inline: true },
+                    )
+                    break;
+                default:
+                    break;
+                }
+            } else {
+                switch (entry.status) {
+                case 'currently_publishing':
+                    message.addFields(
+                        { name: 'Status', value: `Currently publishing`, inline: true },
+                    );
+                    break;
+                case 'currently_airing':
+                    message.addFields(
+                        { name: 'Status', value: `Currently airing`, inline: true },
+                    );
+                    break;
+                default:
+                    break;
+                }
+            }
+
+            if (entry.synopsis.length > 1024) {
+                message.addFields({name: '\u200B', value: `\u200B`, inline: false});
+                message.addFields({name: 'Synopsis', value: `${entry.synopsis.substring(0, 950)}... [Read more](https://myanimelist.net/${interaction.options.getSubcommand()}/${entry.id}#synopsis)`, inline: false});
+            } else {
+                message.addFields({name: '\u200B', value: `\u200B`, inline: false});
+                message.addFields({name: 'Synopsis', value: `${entry.synopsis}`, inline: false});
+            }
+
+            // ================== End Embed Builder ==================
+
+            // await interaction.editReply({ content: `${entry.main_picture.large}\nTitle: ${entry.title}\nRank: ${entry.rank}\nScore: ${entry.mean}\nAlternative Title: ${entry.alternative_title}`, components: [] });
+            await interaction.deleteReply();
+            await interaction.followUp({ embeds: [message] });
         } catch(e) {
             console.error(e);
-            await interaction.editReply({ content: 'No selection was made within a minute.', components: [] });
+            await interaction.deleteReply();
+            await interaction.followUp({ content: 'No selection was made within a minute.', components: [] });
         }
     }
 };
 
+// Searches for the anime or manga using a title specified in the searchParams
 async function search(searchParams, searchAmount, animeOrManga) {
     const params = new URLSearchParams();
     params.append('q', searchParams);
-    params.append('limit', searchAmount);
-    params.append('fields', 'rank,mean,alternative_title');
+    params.append('limit', 10);
     console.log("https://api.myanimelist.net/v2/" + animeOrManga + params);
     return new Promise((resolve, reject) => {
         fetch('https://api.myanimelist.net/v2/' + animeOrManga + params, {
@@ -124,9 +198,13 @@ async function search(searchParams, searchAmount, animeOrManga) {
     })
 };
 
+// Grabs the entry of the anime or manga from the API using the titleID returned from the search function
 async function grabEntry(titleID, animeOrManga) {
     const params = new URLSearchParams();
-    params.append('fields', 'rank,mean,alternative_title');
+    // Add the fields you want to retrieve from the API
+    // Unfortunatly, the API does not allow you to retrieve all fields at once
+    // So this needs to be as long as it currently is :(
+    params.append('fields', 'id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics');
     console.log(`https://api.myanimelist.net/v2/${animeOrManga}/${titleID}`);
     return new Promise((resolve, reject) => {
         fetch(`https://api.myanimelist.net/v2/${animeOrManga}/${titleID}?${params}`, {
@@ -146,3 +224,19 @@ async function grabEntry(titleID, animeOrManga) {
         });
     })
 };
+
+// This currently returns [blob Promise] instead of the image
+async function grabImage(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(response => response.blob())
+        .then(data => {
+            console.log(data);
+            resolve(data);
+        })
+        .catch(error => {
+            console.error(error);
+            reject(error);
+        });
+    })
+}
